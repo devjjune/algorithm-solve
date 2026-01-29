f84bf57a2ad3c1ab1bbe68b6603a37b5b8915e0b
황지윤 <hjy648012@naver.com>
2026-01-29 16:50:10 +0900

    docs(pg): 문제풀이 주석 추가

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -5,3 +5,12 @@ class Solution {
         return (int) answer2;
     }
 }
+// === 권장 풀이 ===
+//class Solution {
+//    public int solution(int num1, int num2) {
+//        // 1. (double)로 형변환하여 소수점까지 계산 - 한 줄 전체에 순차적으로 적용
+//        // 2. 1000을 곱함
+//        // 3. 마지막에 (int)로 캐스팅하여 정수 부분만 추출
+//        return (int) ((double) num1 / num2 * 1000);
+//    }
+//}
