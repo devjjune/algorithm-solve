aaf6efeac2b7e612c3f6a6ec200361615a302485
황지윤 <hjy648012@naver.com>
2026-01-29 16:41:58 +0900

    feat(pg): 프로그래머스 테스트용 클래스 추가

---
+++ src/programmers/Solution.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        // 프로그래머스 예제 입력값 설정
+
+        // 실행 결과 및 출력
+
+    }
+
+   // 프로그래머스 코드
+
+}
