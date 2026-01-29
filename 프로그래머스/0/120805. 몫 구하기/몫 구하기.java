227b8339c07c668f8772378c3f2577783261b10a
황지윤 <hjy648012@naver.com>
2026-01-29 17:13:57 +0900

    docs(pg): 문제 풀이 주석 추가

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -1,9 +1,12 @@
 class Solution {
     public int solution(int num1, int num2) {
+        // 조건 범위를 벗어나면 즉시 종료 (예외 처리)
         if (num1 <= 0 || num1 > 100 || num2 <= 0 || num2 > 100) {
-            return 0;
+            return 0; // or 예외를 던짐
         }
 
         return num1 / num2;
     }
 }
+// java.lang.ArithmeticException: / by zero 에 대한 예외 처리
+// 제한사항이 0 < num1 ≤ 100 와 같은 경우엔 다 쪼개 써야 함
