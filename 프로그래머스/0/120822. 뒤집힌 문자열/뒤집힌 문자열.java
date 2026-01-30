c5f5a1c0b4f3d8c16aa02a50daf3f2554878d556
황지윤 <hjy648012@naver.com>
2026-01-30 11:39:22 +0900

    docs(pg): 문제풀이 주석 추가 및 가독성 개선

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -2,5 +2,15 @@ class Solution {
     public String solution(String my_string) {
         
         return new StringBuilder(my_string).reverse().toString();
+        // 변수에 인스턴스를 담지X (재사용할 필요 없음), 메서드 체이닝 기법 활용
     }
 }
+
+/*
+[ StringBuffer와 StringBuilder 클래스]
+- 내부적으로 문자열 편집을 위한 버퍼(buffer)를 가지고 있어 지정된 문자열을 변경할 수 있다.
+  String과 달리 문자열을 추가(append)하거나 수정해도 새로운 객체를 생성하지 않고 기존 메모리 공간을 활용한다.
+- 주요 메서드: .append(), .insert(), .delete(), .reverse() 등
+- 멀티스레드 환경이 아닌 일반적인 상황에서는 StringBuilder이 더 성능이 좋아 주로 사용한다.
+  반면 StringBuffer은 동기화를 지원하여 멀티스레드 상황에서 안전하다.
+ */
--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -13,24 +13,28 @@ class Solution {
 }
 // 마지막을 else if가 아닌 else로 마무리하며 오류 방지
 
-// === 더 깔끔한 코드 (중복되는 범위 제거) ===
-//class Solution {
-//    public int solution(int angle) {
-//        if (angle < 90) return 1;
-//        if (angle == 90) return 2;
-//        if (angle < 180) return 3;
-//        return 4;
-//    }
-//}
+/*
+=== 더 깔끔한 코드 (중복되는 범위 제거) ===
+class Solution {
+    public int solution(int angle) {
+        if (angle < 90) return 1;
+        if (angle == 90) return 2;
+        if (angle < 180) return 3;
+        return 4;
+    }
+}
+ */
 
-// === switch문 활용 ===
-//public int solution(int angle) {
-//    return switch (angle) {
-//        case 90 -> 2;
-//        case 180 -> 4;
-//        default -> {
-//            if (angle < 90) yield 1;
-//            else yield 3;
-//        }
-//    };
-//}
+/*
+=== switch문 활용 ===
+public int solution(int angle) {
+    return switch (angle) {
+        case 90 -> 2;
+        case 180 -> 4;
+        default -> {
+            if (angle < 90) yield 1;
+            else yield 3;
+        }
+    };
+}
+ */
