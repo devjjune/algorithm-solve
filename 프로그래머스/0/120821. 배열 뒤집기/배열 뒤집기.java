06a356e786ba30f99a9dae41efcc3515fa62afd5
황지윤 <hjy648012@naver.com>
2026-01-30 09:42:05 +0900

    docs(pg): 문제풀이 주석 추가

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,11 +1,14 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];
+        int[] answer = new int[num_list.length]; // 1. 똑같은 크기의 배열 생성
         
         for (int i = 0; i < num_list.length; i ++) {
+            // 2. 새 배열의 앞쪽부터, 원본 배열의 뒤쪽 원소를 하나씩 넣음
             answer[i] = num_list[num_list.length - 1 - i];
         }
         
         return answer;
     }
 }
+
+// 배열 vs 리스트: 크기가 정해져 있다면 처음부터 배열을 선언해서 푸는 것이 자바에서는 훨씬 편하고 성능도 좋다
