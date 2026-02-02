f6ad7b87015139d1363fbf63a2c0a93a26359187
황지윤 <hjy648012@naver.com>
2026-02-02 09:28:01 +0900

    docs(pg): 문제풀이 주석 추가

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -9,3 +9,24 @@ class Solution {
             .collect(Collectors.joining());
     }
 }
+
+/*
+=== StringBuilder 활용 ===
+- 단순한 흐름에서는 for문과 StringBuilder 사용하고 조건이 복잡해질 땐 Stream 사용
+
+class Solution {
+    public String solution(String my_string, int n) {
+
+        // 결과를 담을 가변 크기의 문자열 컨테이너 생성
+        StringBuilder sb = new StringBuilder();
+
+        // 문자열을 문자 배열(char)로 변환하여 하나씩 꺼내기
+        for(char c : my_string.toCharArray()){
+            sb.append(String.valueOf(c).repeat(n));
+        }
+
+        // StringBuilder를 일반 String으로 변환하여 반환
+        return sb.toString();
+    }
+}
+ */
