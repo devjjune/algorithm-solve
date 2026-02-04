2942e99a5bbae29425f4d4dd51d9769555720126
june <154238961+devjjune@users.noreply.github.com>
2026-02-04 11:17:28 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String[] vowels = {"a", "e", "i", "o", "u"};
+        
+        for(String vowel : vowels) {
+            if (my_string.contains(vowel)) {
+                my_string = my_string.replace(vowel, "");
+            }
+        }
+        
+        return my_string;
+    }
+}
