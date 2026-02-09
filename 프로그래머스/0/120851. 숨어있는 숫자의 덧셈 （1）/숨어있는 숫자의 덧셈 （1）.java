50cf1ad51945276e7c42c61127e6bc2839b90109
devjjune <jiyoon.hwang92@gmail.com>
2026-02-09 20:48:04 +0900

    docs: 문제 풀이 주석 추가

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(String my_string) {
         
-        return my_string.chars()
-            .filter(Character :: isDigit)
-            .map(Character :: getNumericValue)
-            .sum();
+        return my_string.chars() // 1. 아스키 번호 흐름 생성 [97(글자 a), 49(글자 1) ...]
+            .filter(Character :: isDigit) // 2. 숫자에 해당하는 번호만 통과 [49, ...]
+            .map(Character :: getNumericValue) // 3. 번호를 진짜 숫자로 변환 [1, ...]
+            .sum(); // 4. 흐름에 있는 모든 숫자를 더함
     }
 }
