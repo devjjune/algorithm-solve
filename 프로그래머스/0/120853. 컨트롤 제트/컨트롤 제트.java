b708015c4f55602a112237094b95a4e20905f552
june <154238961+devjjune@users.noreply.github.com>
2026-03-14 23:40:40 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.41 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        String[] arr = s.split(" ");
+        
+        for (String str : arr) {
+            if (str.equals("Z")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(str));
+            }
+        }
+        
+        int sum = 0;
+        
+        for (int num : stack) {
+            sum += num;
+        }
+        
+        return sum;
+    }
+}
