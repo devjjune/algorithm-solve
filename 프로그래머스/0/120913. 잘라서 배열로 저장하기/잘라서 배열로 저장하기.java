d0c5b1539435debe5ee8d23d8948da8d20be38b6
devjjune <jiyoon.hwang92@gmail.com>
2026-03-22 22:48:01 +0900

    docs: 문제풀이 주석 추가

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -5,10 +5,12 @@ class Solution {
          
         List<String> result = new ArrayList<>();
         
-        for (int i = 0; i < my_str.length(); i += n) {
+        for (int i = 0; i < my_str.length(); i += n) { // i++가 아니라 n씩 증가
             result.add(my_str.substring(i, Math.min(my_str.length(), i + n)));
+            // i ~ i+n 구간을 자르고 싶은데 i+n가 전체 문자열 길이의 범위를 넘을 수 있음
+            // my_str.length()와 i+n 비교해서 더 작은 인덱스까지 자름
         }
         
-        return result.toArray(new String[0]);
+        return result.toArray(new String[0]); // new String[0]로 반환 타입 지정, 그냥 toArray()하면 Object[]로 반환됨
     }
 }
