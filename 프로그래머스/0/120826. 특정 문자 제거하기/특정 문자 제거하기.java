09cadd607497f8bb8fd73f006ca433236461292d
june <154238961+devjjune@users.noreply.github.com>
2026-04-14 23:36:41 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -2,6 +2,7 @@ class Solution {
     public String solution(String my_string, String letter) {
         
         String result = my_string.replace(letter, "");
+        
         return result;
     }
 }
