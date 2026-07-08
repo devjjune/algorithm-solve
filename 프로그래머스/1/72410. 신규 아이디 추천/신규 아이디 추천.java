930f50406ff8f0b1566ecc9f1c9cd8dd8668180a
june <154238961+devjjune@users.noreply.github.com>
2026-07-08 13:17:46 +0900

    [level 1] Title: 신규 아이디 추천, Time: 4.26 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,24 +1,22 @@
 class Solution {
     public String solution(String new_id) {
         
-        String result = new_id.toLowerCase()
-            .replaceAll("[^a-z0-9-_.]", "")
-            .replaceAll("\\.{2,}", ".")
-            .replaceAll("^\\.|\\.$", "");
+        String result = new_id.toLowerCase();
+        result = result.replaceAll("[^a-z0-9\\-_.]", "");
+        result = result.replaceAll("\\.{2,}", ".");
+        result = result.replaceAll("^\\.|\\.$", "");
         
-        if(result.isEmpty()) {
+        if (result.isEmpty()) {
             result = "a";
         }
         
-        if(result.length() >= 16) {
-            result = result.substring(0, 15)
-                .replaceAll("\\.$", "");
+        if (result.length() > 15) {
+            result = result.substring(0, 15);
+            result = result.replaceAll("\\.$", "");
         }
         
-        if(result.length() <= 2) {
-            while(result.length() < 3) {
-                result += result.charAt(result.length() - 1);
-            }
+        while (result.length() < 3) {
+            result += result.charAt(result.length() - 1);
         }
         
         return result;
