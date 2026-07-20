24f6b0d5e0a01891b98ee11d61c0efff2104066f
june <154238961+devjjune@users.noreply.github.com>
2026-07-21 00:08:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 61.40 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,22 +3,26 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         
-        Map<String, Integer> hm = new HashMap<>();
-        
-        for(String p : participant) {
-            hm.put(p, hm.getOrDefault(p, 0) + 1);
-        }
-        
-        for(String c : completion) {
-            hm.put(c, hm.get(c) - 1);
-        }
-        
+        Map<String, Integer> nameCount = new HashMap<>();
         String answer = "";
         
-        for(String key : hm.keySet()) {
-            if (hm.get(key) != 0) {
-                answer = key;
-                break;
+        for (String name : participant) {
+            if (nameCount.containsKey(name)) {
+                int current = nameCount.get(name);
+                nameCount.put(name, current + 1);
+            } else {
+                nameCount.put(name, 1);
+            }
+        }
+        
+        for (String name : completion) {
+            int current = nameCount.get(name);
+            nameCount.put(name, current - 1);
+        }
+        
+        for (String name : nameCount.keySet()) {
+            if (nameCount.get(name) > 0) {
+                answer = name;
             }
         }
         
