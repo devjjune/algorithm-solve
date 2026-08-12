f972507a4c2f294c31104f1cd250ca77b5261655
june <154238961+devjjune@users.noreply.github.com>
2026-08-12 15:06:05 +0900

    [level 2] Title: 주식가격, Time: 23.19 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[prices.length];
+        
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length - 1 - idx;
+        }
+        
+        return answer;
+    }
+}
